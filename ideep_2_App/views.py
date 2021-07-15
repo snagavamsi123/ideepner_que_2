29d35cfacfacbe8aac0d756bb865447424d030f9
snagavamsi123 <snagavamsi123@gmail.com>
2021-07-15 17:55:36 +0530

    normal commit

--- ideep_2_App/views.py
+++ ideep_2_App/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from .models import Customer,Order,Payment
 
-
-
-
 def home(request):
     values1=Customer.objects.all()
     values2 = Order.objects.all()
@@ -11,19 +8,15 @@ def home(request):
     return render(request,'home.html',{'values1':values1,'values2':values2,'values3':values3})
 
 def failed(request):
-    values1=Customer.objects.all()
-    values2 = Order.objects.all()
-    values3=Payment.objects.all()
-
     failed = Customer.objects.raw('''
     select *,SUM(c) as failed_count 
     FROM (select *, count(*) as c 
-            FROM (select * 
-                FROM ((ideep_queries.ideep_2_app_customer as cust 
-                INNER JOIN ideep_queries.ideep_2_app_order AS ord 
-                    ON cust.customer_no=ord.customer_no_id) 
-                INNER JOIN ideep_queries.ideep_2_app_payment AS pay 
-                    ON ord.order_id=pay.order_id_id)) AS outer_table 
+            FROM (  select * 
+                    FROM ((ideep_queries.ideep_2_app_customer as cust 
+                    INNER JOIN ideep_queries.ideep_2_app_order AS ord 
+                        ON cust.customer_no=ord.customer_no_id) 
+                    INNER JOIN ideep_queries.ideep_2_app_payment AS pay 
+                        ON ord.order_id=pay.order_id_id)) AS outer_table 
                 GROUP BY order_id_id,status having (status)='failed') as table2   
                 GROUP BY customer_name ''')
 
@@ -31,6 +24,7 @@ def failed(request):
     return render(request,'Failed.html',{'values':failed})
 
 def total_amount(request): 
+
     values = Customer.objects.raw(
         '''
         select *,SUM(order_amount) as state_amount
@@ -44,6 +38,7 @@ def total_amount(request):
     
 
 def order_count(request):
+
     values = Customer.objects.raw(
         '''
         select *,count(order_id) as count
