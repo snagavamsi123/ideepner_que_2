415da17b4cf4a2606ec466db5a256e0939139117
snagavamsi123 <snagavamsi123@gmail.com>
2021-07-15 16:56:53 +0530

    2nd question fully working

--- ideep_2_App/views.py
+++ ideep_2_App/views.py
@@ -15,10 +15,6 @@ def failed(request):
     values2 = Order.objects.all()
     values3=Payment.objects.all()
 
-    #failed = Payment.objects.filter(status='failed')
-
-    #failed = Payment.objects.raw('select *, count(*) as c FROM ideep_queries.ideep_2_app_payment GROUP BY order_id_id,status having (ideep_queries.ideep_2_app_payment.status)="failed"')
-
     failed = Customer.objects.raw('''
     select *,SUM(c) as failed_count 
     FROM (select *, count(*) as c 
@@ -28,21 +24,34 @@ def failed(request):
                     ON cust.customer_no=ord.customer_no_id) 
                 INNER JOIN ideep_queries.ideep_2_app_payment AS pay 
                     ON ord.order_id=pay.order_id_id)) AS outer_table 
-                GROUP BY order_id_id,status having (status)='failed') as table2 
-                
+                GROUP BY order_id_id,status having (status)='failed') as table2   
                 GROUP BY customer_name ''')
 
            
     return render(request,'Failed.html',{'values':failed})
 
 def total_amount(request): 
-    values = Order.objects.all()
+    values = Customer.objects.raw(
+        '''
+        select *,SUM(order_amount) as state_amount
+		from (ideep_queries.ideep_2_app_customer as cust
+		inner join ideep_queries.ideep_2_app_order as ord on cust.customer_no=ord.customer_no_id)
+        group by State;
+        
+        '''
+    )
     return render(request,'total_amount.html',{'values':values})
     
 
 def order_count(request):
-    values1=Customer.objects.all()
-    values2 = Order.objects.all()
-    values3=Payment.objects.all()
-    return render(request,'order_count.html',{'values1':values1,'values2':values2,'values3':values3})
+    values = Customer.objects.raw(
+        '''
+        select *,count(order_id) as count
+		from ((ideep_queries.ideep_2_app_customer as cust
+		inner join ideep_queries.ideep_2_app_order as ord on cust.customer_no=ord.customer_no_id))
+        group by customer_name;
+        
+        '''
+    )
+    return render(request,'order_count.html',{'values':values})
     
