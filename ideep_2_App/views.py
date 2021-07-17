964153b3a93e7372e6eac8f948cf62d93d89b19a
snagavamsi123 <snagavamsi123@gmail.com>
2021-07-17 13:27:13 +0530

    Fully completed

--- ideep_2_App/views.py
+++ ideep_2_App/views.py
@@ -53,13 +53,48 @@ def order_count(request):
 
 
 def add_order(request):
+    if request.method=='POST':
+        form1=OrderForm(request.POST)
+        if form1.is_valid():
+            form1.save()
+            message = 'Successfully Saved'
+            return render(request,'add_order.html',{'form':form1,'message':message}) 
+        else:
+            message = 'Validation Failed'
+            return render(request,'add_order.html',{'form':form1,'message':message}) 
+
     form1 = OrderForm
     return render(request,'add_order.html',{'form':form1})
 
 def add_customer(request):
+    if request.method=='POST':
+        form2=CustomerForm(request.POST)
+        name = request.POST['customer_name']
+        city_name = request.POST['City']
+        form = Customer.objects.all()
+        for data in form:
+            if data.customer_name==name and data.City==city_name:
+                message = 'User Already exist Failed'
+                return render(request,'add_customer.html',{'form':form2,'message':message}) 
+        if form2.is_valid():
+            form2.save()
+            message = 'Successfully Saved'
+            return render(request,'add_customer.html',{'form':form2,'message':message}) 
+        else:
+            message = 'Validation Failed'
+            return render(request,'add_customer.html',{'form':form2,'message':message}) 
     form2=CustomerForm
     return render(request,'add_customer.html',{'form':form2})
 
 def payment(request):
+    if request.method=='POST':
+        form3=PaymentForm(request.POST)
+        if form3.is_valid():
+            form3.save()
+            message = 'Successfully Saved'
+            return render(request,'add_payment.html',{'form':form3,'message':message}) 
+        else:
+            message = 'Validation Failed'
+            return render(request,'add_payment.html',{'form':form3,'message':message}) 
     form3=PaymentForm
     return render(request,'add_payment.html',{'form':form3})
